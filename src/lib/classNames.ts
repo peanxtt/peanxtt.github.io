5cca74fd0d0dbb3ef6bbe89e5ee845f62645e712
Hua Sheng <94678908+peanxtt@users.noreply.github.com>
2024-09-13 00:55:58 +0800

    build: remove cache

--- src/lib/classNames.ts
+++
@@ -1,8 +0,0 @@
-export default function classNames(...classes: (string | boolean | undefined | null)[]) {
-  return classes.filter(Boolean).join(' ')
-    .replace(/\s\s+/g, ' ').trim()
-}
-
-export function sanitizeClassName(className?: string | null) {
-  return className?.replace(/[~@'"<>]/g, '')
-}
