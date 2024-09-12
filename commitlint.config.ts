8d6c585f50d20bafdd2ad3f82a0e0baf4b55020a
Hua Sheng <94678908+peanxtt@users.noreply.github.com>
2024-09-12 14:08:43 +0800

    commitlint

--- commitlint.config.ts
+++ commitlint.config.ts
@@ -4,7 +4,6 @@ import {RuleConfigSeverity} from '@commitlint/types';
 const Configuration: UserConfig = {
 
   extends: ['@commitlint/config-conventional'],
-  parserPreset: 'conventional-changelog-atom',
   formatter: '@commitlint/format',
   rules: {
     'type-enum': [RuleConfigSeverity.Error, 'always', ['foo']], 
